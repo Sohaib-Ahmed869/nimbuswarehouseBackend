b583adc303c0e9608a56642f69ea2161201ad573
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-07-14 16:27:31 +0500

    updates

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const warehouseRoutes = require("./Routes/warehouseRoutes");
 const emailRoutes = require("./Routes/emailRoute");
 const cashierRoutes = require("./Routes/cashierRoutes");
 const https = require("https");
-// const fs = require("fs");
+const fs = require("fs");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -21,19 +21,21 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Connect to MongoDB
-// mongoose
-//   .connect("mongodb+srv://hexlertech:vQEmfMxnymZ510vo@cluster0.gyfkxge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log(err));
-
 mongoose
-  // .connect("mongodb://127.0.0.1:27017/warehouse", {
-  .connect("mongodb://127.0.0.1:27017/development", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://hexlertech:vQEmfMxnymZ510vo@cluster0.gyfkxge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Error: ", err));
+  .catch((err) => console.log(err));
+
+// mongoose
+//   // .connect("mongodb://127.0.0.1:27017/warehouse", {
+//   .connect("mongodb://127.0.0.1:27017/development", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log("Error: ", err));
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
@@ -55,10 +57,10 @@ app.use("/products", productRoutes);
 app.use("/warehouses", warehouseRoutes);
 
 // // SSL options
-// const options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/nimbus360.org/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/nimbus360.org/fullchain.pem')
-// };
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/nimbus360.org/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/nimbus360.org/fullchain.pem"),
+};
 
 // Create HTTPS server
 https.createServer(options, app).listen(PORT, () => {
