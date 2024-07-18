de2bb37ff751716ce19185dc20c9b33d5d27a111
Musa <musaharoon.2003@gmail.com>
2024-07-18 13:05:36 +0500

    .

--- server.js
+++ server.js
@@ -20,27 +20,27 @@ const PORT = process.env.PORT || 3001;
 app.use(cookieParser());
 app.use(express.json());
 
-// // Connect to MongoDB
-// mongoose
-//   .connect("mongodb+srv://hexlertech:vQEmfMxnymZ510vo@cluster0.gyfkxge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log(err));
-
+// Connect to MongoDB
 mongoose
-  // .connect("mongodb://127.0.0.1:27017/warehouse", {
-  .connect("mongodb://127.0.0.1:27017/development", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb+srv://hexlertech:vQEmfMxnymZ510vo@cluster0.gyfkxge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Error: ", err));
-app.use(express.json({ limit: "50mb" }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://nimbus360.org"],
-    credentials: true,
-  })
-);
+  .catch((err) => console.log(err));
+
+// mongoose
+//   // .connect("mongodb://127.0.0.1:27017/warehouse", {
+//   .connect("mongodb://127.0.0.1:27017/development", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log("Error: ", err));
+// app.use(express.json({ limit: "50mb" }));
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://nimbus360.org"],
+//     credentials: true,
+//   })
+// );
 
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 console.log("timezone: ", timezone);
@@ -57,17 +57,17 @@ app.use("/email", emailRoutes);
 app.use("/products", productRoutes);
 app.use("/warehouses", warehouseRoutes);
 
-// // SSL options
-// const options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/nimbus360.org/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/nimbus360.org/fullchain.pem')
-// };
+// SSL options
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/nimbus360.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/nimbus360.org/fullchain.pem')
+};
 
-// // Create HTTPS server
-// https.createServer(options, app).listen(PORT, () => {
-//   console.log(`HTTPS Server is running on port ${PORT}`);
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Create HTTPS server
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`HTTPS Server is running on port ${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
